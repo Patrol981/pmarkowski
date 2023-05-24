67e1863403c44436e491d8c498da7728c97bd377
Patryk Markowski <patrol56789@gmail.com>
2023-05-24 11:55:13 +0200

    Updates

---
+++ _app/immutable/chunks/1.4c61bfdc.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.89d9bf04.js";export{t as component};
--- _app/immutable/chunks/1.e92f9603.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.7884db5a.js";export{t as component};
